2c7a45e208ea6a404f693c4e506a5ac5954e70ab
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-03-05 19:09:48 -0500

    add a simple path search interface

--- app.js
+++ app.js
@@ -154,7 +154,25 @@ require(['jquery', 'd3', './listeners', './listview', './setlist', './overviewgr
           $.getJSON(this.value, function (paths) {
             loadPaths(paths);
           });
+        });
 
+        $('#submit').on('click', function() {
+          var s= $('#from_node').val();
+          var t= $('#to_node').val();
+          var k = $('#at_most_k').val();
+          var $this = $(this).attr({
+            disabled: 'disabled'
+          }).html('<i class="fa fa-spinner fa-pulse"></i>');
+          searchPaths(s, t, k, function(path) {
+            console.log('got path',path);
+          }).then(function (paths) {
+            paths = JSON.parse(paths);
+            console.log('got paths',paths);
+            $this = $this.attr({
+              disabled: null
+            }).text('Search');
+            loadPaths(paths);
+          });
         });
 
         $.getJSON("testpaths1.json", function (paths) {
@@ -283,6 +301,62 @@ require(['jquery', 'd3', './listeners', './listview', './setlist', './overviewgr
     )
     ;
 
+    function getIncrementalJSON(url, data, onChunkDone) {
+      var processed = 0;
+      function sendChunk(path) {
+        path = JSON.parse(path);
+        onChunkDone(path);
+      }
+      function processPart(text) {
+        var p = 0, act = 0, open = 0,
+          l = text.length,
+          c, start;
+        if (text[act] === '[') { //skip initial [
+          act = 1;
+        }
+        start = act;
+        while(act < l) {
+          c = text[act];
+          if (c === '{') { //starting object
+            open += 1;
+          } else if (c === '}') { //closing object
+            open -= 1;
+            if (open === 0) { //found a full chunk
+              sendChunk(text.substring(start, act+1));
+              start = act + 1;
+              if (text[start] === ',') { //skip ,
+                start += 1;
+                act += 1;
+              }
+            }
+          }
+          act++;
+        }
+        return start; //everything before start was fully processed
+      }
+      return $.ajax({
+        async: true,
+        url : url,
+        data: data,
+        dataType : 'text',
+        success: function(data) {
+          processPart(data.substr(processed))
+        },
+        xhr: function(){
+          // get the native XmlHttpRequest object
+          var xhr = $.ajaxSettings.xhr() ;
+          // set the onprogress event handler
+          xhr.onprogress = function(evt){
+            processed += processPart(evt.currentTarget.responseText.substr(processed));
+          } ;
+          return xhr ;
+        }
+      })
+    }
+
+    function searchPaths(source, target, k, onPathDone) {
+      return getIncrementalJSON('/api/pathway/path/'+source+'/'+target, { k : k || 10 }, onPathDone);
+    }
     //$.get("/api/pathway/path", function (resp) {
     //
     //  var paths = JSON.parse(resp);
