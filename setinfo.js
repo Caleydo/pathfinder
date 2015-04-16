3663e02e4a15269954df19772322376431eb5003
Christian Partl <partl@icg.tugraz.at>
2015-04-16 13:57:29 +0200

    switched colorscheme for sets to avoid conflict with selection

--- setinfo.js
+++ setinfo.js
@@ -3,7 +3,15 @@
  */
 define(['d3', './listeners', './config'], function (d3, listeners, config) {
 
-  var scale = d3.scale.category10();
+  var colorScheme = Object.create(colorbrewer.Set1[9]);
+  //remove orange color to avoid conflict with highlighting
+  colorScheme.splice(4, 1);
+  //var scheme = Object.create(d3.scale.category10().domain());
+  //scheme.splice(1, 1);
+
+  var scale = d3.scale.ordinal()
+    .domain(d3.range(8))
+    .range(colorScheme);
   var numTypes = -1;
 
   return {
