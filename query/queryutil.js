31885b29aaf4dba099a92f6937c77cf735fc632d
Christian Partl <partl@icg.tugraz.at>
2015-08-26 13:57:21 +0200

    added context entries for setting start and end node, wip

--- query/queryutil.js
+++ query/queryutil.js
@@ -1,46 +1,61 @@
 define(['d3', './queryview', '../listoverlay', '../uiutil', '../setinfo', '../config'], function (d3, queryView, ListOverlay, uiUtil, setInfo, config) {
 
-  //var listOverlay = new ListOverlay();
+    //var listOverlay = new ListOverlay();
 
-  return {
-    getFilterOverlayItems: function (constraintType, text) {
-      function getConstraintInfo() {
-        switch (constraintType) {
-          case "name":
-            return {label: text, value: text, category: "Name"};
-            break;
-          case "type":
-            return {label: text, value: text, category: "Type"};
-            break;
-          case "set":
-            return {
-              label: setInfo.getSetLabel(text),
-              value: text,
-              id: text,
-              setNodeLabel: config.getSetNodeLabel(setInfo.get(text)),
-              category: "Set"
-            };
-            break;
-          default:
-            return;
-        }
-      }
+    return {
+        getFilterOverlayItems: function (constraintType, text) {
+            function getConstraintInfo() {
+                switch (constraintType) {
+                    case "name":
+                        return {label: text, value: text, category: "Name"};
+                        break;
+                    case "type":
+                        return {label: text, value: text, category: "Type"};
+                        break;
+                    case "set":
+                        return {
+                            label: setInfo.getSetLabel(text),
+                            value: text,
+                            id: text,
+                            setNodeLabel: config.getSetNodeLabel(setInfo.get(text)),
+                            category: "Set"
+                        };
+                        break;
+                    default:
+                        return;
+                }
+            }
 
-      return [{
-        text: "Must contain",
-        icon: "\uf0b0",
-        callback: function () {
-          queryView.addNodeFilter(getConstraintInfo(), false);
+            return [
+                {
+                    text: "Must contain",
+                    icon: "\uf0b0",
+                    callback: function () {
+                        queryView.addNodeFilter(getConstraintInfo(), false);
+                    }
+                },
+                {
+                    text: "Must not contain",
+                    icon: "\uf0b0",
+                    callback: function () {
+                        queryView.addNodeFilter(getConstraintInfo(), true);
+                    }
+                },
+                {
+                    text: "As start",
+                    icon: "\uf0b0",
+                    callback: function () {
+                        queryView.addNodeFilter(getConstraintInfo(), false);
+                    }
+                },
+                {
+                    text: "As end",
+                    icon: "\uf0b0",
+                    callback: function () {
+                        queryView.addNodeFilter(getConstraintInfo(), true);
+                    }
+                }
+            ]
         }
-      },
-        {
-          text: "Must not contain",
-          icon: "\uf0b0",
-          callback: function () {
-            queryView.addNodeFilter(getConstraintInfo(), true);
-          }
-        }
-      ]
     }
-  }
 });
